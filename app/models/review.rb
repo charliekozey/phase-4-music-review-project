3d1e768baf82970bfecc7ccf3ea5604973c3e801
Spencer Johnson <sljohnson2222@gmail.com>
2022-04-19 09:26:23 -0500

    method to query highest rated review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :album
+
+  def self.highest_rated
+    #returns array of album ratings, highest to lowest
+    Review.all.sort_by {|r| -r.rating }.first(2) 
+    #need to change to 5? instead of 2
+  end
 end
