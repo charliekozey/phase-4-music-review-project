c8c581a33fd9465cecbb0092ed978b8328d7fd48
Michael Dally <mddally@gmail.com>
2022-04-20 11:05:29 -0400

    I think i did auth shit idk

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+    skip_before_action :authorized, only: :get_highest_rated_albums
     def get_highest_rated_albums
         top_albums = Review.highest_rated
         render json: top_albums 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    skip_before_action :authorized, only: :create
+    skip_before_action :authorized, only: [:create, :show]
 
     def create
         user = User.create!(user_params)
