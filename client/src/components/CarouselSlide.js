5832550e656dcbdd5be20979670138e7653ec8bb
Spencer Johnson <sljohnson2222@gmail.com>
2022-04-21 08:56:15 -0500

    carousel working with link

--- client/src/components/CarouselSlide.js
+++ client/src/components/CarouselSlide.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"
+// import { useHistory } from 'react-router-dom'
+
 
 function CarouselSlide({ topAlbum }) {
     console.log( topAlbum )
+    // let history = useHistory();
+
+    // function handleClick() {
+    //     history.push(`albums/${topAlbum.id}`)
+    // }
+
 
     return (
         <div>
+            <Link to={`albums/${topAlbum.id}`}>
             <img alt="" src={ topAlbum.album_art_url } />
             <p className="carousel-artist">{ topAlbum.artist }</p>
             <p className="carousel-title">{ topAlbum.title }</p>
+            </Link>
         </div>
     )
 }
